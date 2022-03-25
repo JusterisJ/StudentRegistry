bda38857d6a4b186a1b7df561305a88541f4ae55
JusterisJ <96180653+JusterisJ@users.noreply.github.com>
2022-03-24 23:49:21 -0700

    Delete StudentsList.jsx

--- StudentList/StudentsList.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Student from "./Student";
-
-export default function StudentsList(props) {
-  const newData = props.data.map((data) => {
-    return <Student name={data.name} surname={data.surname} dob={data.dob} city={data.city} program={data.program} group={data.group} key={data.id} updateStudentsList={props.updateStudentsList} />;
-  });
-  return <>{newData}</>;
-}
